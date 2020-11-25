100d691a9755cc81386d34c0632e26e79c2e82ff
Acalltoredemption <acalltoredemption@gmail.com>
2020-11-24 16:28:43 -0800

    began implementing axios fetch integration with unsplash api to fetch image results

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 
 
 class App extends React.Component {
     onSearchSubmit(term){
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+        params: {query: term},
+        headers: {
+            Authorization: 'Client-ID -lMTV76hUCLc96iFWrpndY58hIr1Zt1AHbZftjVt-bc'
+        }
+        });
     }
 
 
